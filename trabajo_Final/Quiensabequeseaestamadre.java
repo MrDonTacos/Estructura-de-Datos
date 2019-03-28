6dd2f6fc4812ff98d696761882655fe24d860b22
Saul Cañedo <45052379+MrDonTacos@users.noreply.github.com>
2019-03-28 06:32:48 -0700

    Agregación de 2 eventos
    Agregando evento prestamos de alumno y profesor

--- trabajo_Final/Quiensabequeseaestamadre.java
+++ trabajo_Final/Quiensabequeseaestamadre.java
@@ -21,7 +21,7 @@ public class Quiensabequeseaestamadre {
 	Laboratorio lab03 = new Laboratorio(3);
 	Laboratorio lab04 = new Laboratorio(4);
 	Laboratorio lab05 = new Laboratorio(5);
-	
+	Laboratorio todosLaboratorios[] = {lab01, lab02, lab03, lab04, lab05};
 	for (int j = 0; j < a.length; j++) {
 			a [j] = new Computadora("AutoCad");
 			c [j] = new Computadora();
@@ -84,19 +84,25 @@ public class Quiensabequeseaestamadre {
 	switch (rd.nextInt(100)+1) { //Lo que está dentro del switch, te va a arrojar un número del 1-100, y lo vamos a trabajar como un número de porcentaje 100%
 	case 1:	case 2:	case 3:	case 4:	case 5:	case 6:	
 	case 7:	case 8:	case 9:	case 10: case 11: case 12:
+		//Aquí va el código para la llegada de los alumnos 			12% [1-12]
+		break;
 	case 13: case 14: case 15: case 16: case 17: case 18:	
 	case 19: case 20: case 21: case 22:	case 23: case 24:	
-		//Aquí va el código para la llegada de los alumnos 			24% [1-24]
+		//Aquí va el código de prestamo de maquina
 		
 		break;	
 	
 	case 25: case 26 : case 27 : case 28 : case 29 : case 30 : 
 	case 31 : case 32: case 33 : case 34 : case 35 : case 36 : 
+		//Aquí va el código del prestamo de Laboratorio;
+		cola.quitar().getDato().apartarLaboratorio(todosLaboratorios[rd.nextInt(5)], rd.nextInt(9)+1);
+		break;
 	case 37 : case 38 : case 39 : case 40 : case 41  :case 42 : 
 	case 43 : case 44  :
-			//Aquí va el código de llegada del profe 				19% [25-44]
+			//Aquí va el código de llegada del profe 				8% [25-44]
 			cola.insertarCola(new Nodo(new Maestro(strAleatorio.numInventarioAleatoria(10))));
-			System.out.println(cola.quitar().getDato().getNumEmpleado());
+			cola.imprimirLista();
+			System.out.println("\n");
 		break;
 		
 		
